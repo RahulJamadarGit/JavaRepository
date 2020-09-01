fa2851283124c32735f4cd75d62760b084b2d0bf
Rahul Jamadar <alwaysjamadar@gmail.com>
2020-09-01 15:55:44 +0530

    Arithmetic Operation Added

--- HelloWorldDemo/src/ms/com/HelloWorldMain.java
+++ HelloWorldDemo/src/ms/com/HelloWorldMain.java
@@ -4,9 +4,28 @@ public class HelloWorldMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Hello World!!");
 
+		System.out.println("Hello World!!");
+		System.out.println("Addition is: " + add(10, 20));
+		System.out.println("Subtraction is: " + subtract(40, 10));
+		System.out.println("Multiplicaton is: " + multiply(40, 10));
+		System.out.println("Division is: " + divide(50, 10));
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public static int divide(int a, int b) {
+		return a / b;
 	}
 
 }
